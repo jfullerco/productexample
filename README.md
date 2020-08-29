2b94de1e3c882d0fdc2e475042f6308881eceb7c
jfullerco <jonathan@jfuller.co>
2020-08-29 18:27:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# productexample
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/productexample)
---
+++ src/comp/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: "1",
+    name: "product 1",
+    desc: "product 1 descrpition"
+
+  },
+  {
+    id: "2",
+    name: "product 2",
+    desc: "product 2 descrpition"
+
+
+  },
+  {
+    id: "3",
+    name: "product 3",
+    desc: "product 3 descrpition"
+
+
+  },
+  {
+    id: "4",
+    name: "product 4",
+    desc: "product 4 descrpition"
+
+
+  }
+
+
+
+]
